220f6652a7a6b64b9f0fb530eaa85d4338ad777e
Denisaa <denisafleancu2609@gmail.com>
2023-11-02 13:03:59 +0200

    CARWASH-1 Created custom button widget

---
+++ car_wash/lib/widgets/custom_button.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton(
+      {super.key,
+      required this.onTap,
+      required this.withGradient,
+      required this.text,
+      this.colorGradient1,
+      this.colorGradient2,
+      this.width,
+      this.height,
+      this.color,
+      this.textColor,
+      this.child,
+      required this.rowText});
+
+  final void Function() onTap;
+  final bool withGradient;
+  final Color? colorGradient1;
+  final Color? colorGradient2;
+  final String text;
+  final double? width;
+  final double? height;
+  final Color? color;
+  final Color? textColor;
+  final Row? child;
+  final bool rowText;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+        child: Container(
+          padding: const EdgeInsets.only(left: 20, right: 10),
+          height: height ?? 50,
+          width: width ?? MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              gradient: withGradient
+                  ? LinearGradient(
+                      colors: [
+                        colorGradient1!,
+                        colorGradient2!,
+                      ],
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
+                    )
+                  : null,
+              color: !withGradient ? color ?? Colors.white : null,
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.black.withOpacity(0.25),
+                    spreadRadius: 0,
+                    blurRadius: 4,
+                    offset: const Offset(0, 4))
+              ]),
+          child: rowText
+              ? child
+              : Center(
+                  child: Text(
+                  text,
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: withGradient
+                          ? Colors.white
+                          : textColor ?? Colors.black),
+                )),
+        ),
+      ),
+    );
+  }
+}
