71c817184694cea052165d1c8fcce7b741d32c02
Denisaa <denisafleancu2609@gmail.com>
2023-11-23 16:28:29 +0200

    CARWASH-6 Login functionality & network connection

--- car_wash/lib/screens/login_screen.dart
+++ car_wash/lib/screens/login_screen.dart
@@ -1,9 +1,14 @@
 import 'dart:ui';
 
 import 'package:car_wash/screens/signup_screen.dart';
+import 'package:car_wash/screens/start_screen.dart';
+import 'package:car_wash/services/auth.dart';
 import 'package:car_wash/widgets/custom_button.dart';
 import 'package:car_wash/widgets/custom_entry_field.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -19,6 +24,93 @@ class _LoginScreenState extends State<LoginScreen> {
 
   String? emailError;
   String? passwordError;
+
+  var isDeviceConnected = false;
+  bool isAlertSet = false;
+
+  Future<void> checkInternetConnection() async {
+    isDeviceConnected = await InternetConnectionChecker().hasConnection;
+    if (!isDeviceConnected && !isAlertSet) {
+      showDialogBox();
+      setState(() => isAlertSet = true);
+    }
+  }
+
+  showDialogBox() => showCupertinoDialog<String>(
+        context: context,
+        builder: (BuildContext context) => CupertinoAlertDialog(
+          title: const Text('No Internet Connection'),
+          content: const Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.wifi_off,
+                color: Color.fromARGB(255, 157, 157, 157),
+                size: 50,
+              ),
+              Text('Please check your internet connection and try again!'),
+            ],
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () async {
+                Navigator.pop(context, 'Cancel');
+                setState(() => isAlertSet = false);
+                isDeviceConnected =
+                    await InternetConnectionChecker().hasConnection;
+                if (!isDeviceConnected && !isAlertSet) {
+                  showDialogBox();
+                  setState(() => isAlertSet = true);
+                }
+              },
+              child: const Text('Retry!'),
+            ),
+          ],
+        ),
+      );
+
+  Future<void> logIn() async {
+    emailError = null;
+    passwordError = null;
+
+    if (_controllerEmail.text.isEmpty) {
+      setState(() {
+        emailError = "Please enter your Email Address!";
+      });
+      return;
+    }
+    if (_controllerPassword.text.isEmpty) {
+      setState(() {
+        passwordError = "Please enter your Password!";
+      });
+      return;
+    }
+    try {
+      await Auth()
+          .signWithEmailAndPassword(
+              email: _controllerEmail.text, password: _controllerPassword.text)
+          .then((value) {
+        Navigator.popUntil(context, (route) => route.isFirst);
+        Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (context) => const StartScreen()));
+      });
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        switch (e.code) {
+          case 'weak-password':
+            passwordError = 'Password should be at least 6 characters';
+            break;
+          case 'email-is-already-in-use':
+            emailError = 'The Email has already been registered!';
+            break;
+          case 'invalid-email':
+            emailError = 'Your Email Address is invalid!';
+            break;
+        }
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -120,7 +212,13 @@ class _LoginScreenState extends State<LoginScreen> {
                           horizontal: 15,
                         ),
                         child: CustomButton(
-                          onTap: () {},
+                          onTap: () async {
+                            isAlertSet = false;
+                            await checkInternetConnection();
+                            if (isDeviceConnected) {
+                              logIn();
+                            }
+                          },
                           withGradient: false,
                           text: "Log In",
                           rowText: false,
