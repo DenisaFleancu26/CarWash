e3f171efa508b5bf24c479f4a5cc627c483740bb
Denisaa <denisafleancu2609@gmail.com>
2023-11-29 14:48:28 +0200

    CARWASH-13 Created NavBar

--- car_wash/lib/screens/profile_screen.dart
+++ car_wash/lib/screens/profile_screen.dart
@@ -1,7 +1,11 @@
 import 'package:car_wash/screens/change_password.dart';
+import 'package:car_wash/screens/home_screen.dart';
 import 'package:car_wash/screens/login_screen.dart';
+import 'package:car_wash/screens/map_screen.dart';
+import 'package:car_wash/screens/signup_screen.dart';
 import 'package:car_wash/services/auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -16,6 +20,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
   final User? user = Auth().currentUser;
   String username = '';
   String email = '';
+  int index = 2;
 
   @override
   void initState() {
@@ -99,9 +104,47 @@ class _ProfileScreenState extends State<ProfileScreen> {
     );
   }
 
+  final items = <Widget>[
+    Icon(Icons.map_rounded, size: 30),
+    Icon(Icons.home, size: 30),
+    Icon(Icons.account_circle, size: 30),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBody: true,
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Colors.transparent,
+        color: const Color.fromARGB(255, 255, 255, 255),
+        animationDuration: const Duration(milliseconds: 300),
+        height: 45,
+        index: index,
+        items: items,
+        onTap: (index) => setState(() {
+          this.index = index;
+          switch (index) {
+            case 0:
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => const MapScreen()),
+              );
+              break;
+            case 1:
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => const HomeScreen()),
+              );
+              break;
+            case 2:
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => const ProfileScreen()),
+              );
+              break;
+          }
+        }),
+      ),
       body: SingleChildScrollView(
         child: Container(
           height: MediaQuery.of(context).size.height,
