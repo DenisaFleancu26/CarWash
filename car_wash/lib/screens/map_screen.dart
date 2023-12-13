d92fcfd15653076bd1eba71d1270ae41639b3485
Denisaa <denisafleancu2609@gmail.com>
2023-12-13 13:40:21 +0200

    CARWASH-12 Added Google map

--- car_wash/lib/screens/map_screen.dart
+++ car_wash/lib/screens/map_screen.dart
@@ -1,9 +1,13 @@
+import 'dart:async';
+
 import 'package:car_wash/screens/home_screen.dart';
 import 'package:car_wash/screens/login_screen.dart';
 import 'package:car_wash/screens/profile_screen.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import '../services/auth.dart';
 
@@ -18,55 +22,91 @@ class _MapScreenState extends State<MapScreen> {
   int index = 0;
   final User? user = Auth().currentUser;
 
+  CameraPosition _cameraPosition = CameraPosition(
+    target: LatLng(45.7494, 21.2272),
+    zoom: 13,
+  );
+  final Completer<GoogleMapController> _controller = Completer();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   final items = const <Widget>[
     Icon(Icons.map_rounded, size: 30),
     Icon(Icons.home, size: 30),
     Icon(Icons.account_circle, size: 30),
   ];
 
+  Future<Position> getUserCurrentLocation() async {
+    try {
+      return await Geolocator.getCurrentPosition();
+    } catch (e) {
+      print("Error getting location: $e");
+      return Future.error("Error getting location");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      extendBody: true,
-      bottomNavigationBar: CurvedNavigationBar(
-        backgroundColor: Colors.transparent,
-        color: const Color.fromARGB(255, 255, 255, 255),
-        animationDuration: const Duration(milliseconds: 300),
-        height: 45,
-        index: index,
-        items: items,
-        onTap: (index) => setState(() {
-          this.index = index;
-          switch (index) {
-            case 0:
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(builder: (context) => const MapScreen()),
-              );
-              break;
-            case 1:
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(builder: (context) => const HomeScreen()),
-              );
-              break;
-            case 2:
-              if (user != null) {
+        extendBody: true,
+        bottomNavigationBar: CurvedNavigationBar(
+          backgroundColor: Colors.transparent,
+          color: const Color.fromARGB(255, 255, 255, 255),
+          animationDuration: const Duration(milliseconds: 300),
+          height: 45,
+          index: index,
+          items: items,
+          onTap: (index) => setState(() {
+            this.index = index;
+            switch (index) {
+              case 0:
                 Navigator.pushReplacement(
                   context,
-                  MaterialPageRoute(
-                      builder: (context) => const ProfileScreen()),
+                  MaterialPageRoute(builder: (context) => const MapScreen()),
                 );
-              } else {
+                break;
+              case 1:
                 Navigator.pushReplacement(
                   context,
-                  MaterialPageRoute(builder: (context) => const LoginScreen()),
+                  MaterialPageRoute(builder: (context) => const HomeScreen()),
                 );
-              }
-              break;
-          }
-        }),
-      ),
-    );
+                break;
+              case 2:
+                if (user != null) {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const ProfileScreen()),
+                  );
+                } else {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const LoginScreen()),
+                  );
+                }
+                break;
+            }
+          }),
+        ),
+        body: Stack(
+          children: [
+            GoogleMap(
+              initialCameraPosition: _cameraPosition,
+              mapType: MapType.normal,
+              zoomControlsEnabled: false,
+              myLocationEnabled: true,
+              myLocationButtonEnabled: true,
+              compassEnabled: false,
+              padding: const EdgeInsets.only(bottom: 60, top: 30),
+              onMapCreated: (GoogleMapController controller) {
+                _controller.complete(controller);
+              },
+            ),
+          ],
+        ));
   }
 }
