b0c951e899ef8e66b6ac21fc94cc726ce6e22b7f
Denisaa <denisafleancu2609@gmail.com>
2024-02-11 19:50:20 +0200

    CARWASH-22 Saved user token

--- car_wash/lib/controllers/notification_controller.dart
+++ car_wash/lib/controllers/notification_controller.dart
@@ -1,6 +1,25 @@
+import 'package:car_wash/controllers/auth_controller.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
 class NotificationController {
+  Future<void> saveToken() async {
+    await FirebaseMessaging.instance.getToken().then((token) async {
+      QuerySnapshot querySnapshot = await FirebaseFirestore.instance
+          .collection('UserTokens')
+          .where('token', isEqualTo: token)
+          .get();
+
+      if (querySnapshot.docs.isEmpty) {
+        await FirebaseFirestore.instance
+            .collection('UserTokens')
+            .doc(AuthController().currentUser?.uid ??
+                FirebaseFirestore.instance.collection('UserTokens').doc().id)
+            .set({'token': token});
+      }
+    });
+  }
+
   Future<void> initNotification() async {
     FirebaseMessaging messaging = FirebaseMessaging.instance;
     await messaging.requestPermission();
